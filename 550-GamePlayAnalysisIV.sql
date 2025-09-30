a01955fa43452f30b0ffc09a8b9cee3929153964
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-30 09:24:21 +0100

    Create 550-GamePlayAnalysisIV.sql

---
+++ 550-GamePlayAnalysisIV.sql
@@ -0,0 +1,20 @@
+WITH FirstLogin AS (
+    SELECT 
+        player_id,
+        MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+),
+NextDayLogin AS (
+    SELECT DISTINCT f.player_id
+    FROM FirstLogin f
+    JOIN Activity a
+      ON f.player_id = a.player_id
+     AND a.event_date = DATEADD(day, 1, f.first_date)
+)
+SELECT 
+    ROUND(CAST(COUNT(DISTINCT n.player_id) AS DECIMAL) 
+          / CAST(COUNT(DISTINCT f.player_id) AS DECIMAL), 2) AS fraction
+FROM FirstLogin f
+LEFT JOIN NextDayLogin n
+  ON f.player_id = n.player_id;
