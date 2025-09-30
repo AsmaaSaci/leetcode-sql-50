d72b52f812b977a1ed4bf2ca665c9538dfb7bea0
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-30 08:36:21 +0100

    Create 1321-RestaurantGrowth.sql

---
+++ 1321-RestaurantGrowth.sql
@@ -0,0 +1,16 @@
+SELECT visited_on,
+       SUM(amount) AS amount,
+       ROUND(SUM(amount) / 7, 2) AS average_amount
+FROM (
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+) c1
+JOIN (
+    SELECT DISTINCT visited_on
+    FROM Customer
+) c2
+ON c1.visited_on BETWEEN DATE_SUB(c2.visited_on, INTERVAL 6 DAY) AND c2.visited_on
+GROUP BY c2.visited_on
+HAVING COUNT(DISTINCT c1.visited_on) = 7
+ORDER BY visited_on;
